051fe22cacfff6e6b8fe5d2843efa45c93ca553f
yuukikawano <heyeyuxi9@gmail.com>
2023-11-29 16:01:34 +0900

    h

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,10 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
+    redirect_to tasks_path
   end
+
+
 
   def destroy
     @task = Task.find(params[:id])
